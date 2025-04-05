b626839221a910b4cd3c4e7303fec65e7b67c323
maisonneuvem6 <151473911+maisonneuvem6@users.noreply.github.com>
2025-04-05 19:34:38 +0000

    Update functions with **arr update, and improved report print out layout

--- DB.c
+++ DB.c
@@ -4,20 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Remove this for merge
-// #define tableSize 5
-
-// IMPORTANT: DB.h longitude name needs to be fixed for this code to work.
-
 DataBase *Db;
-
-// Marek's Functions:
-//      - sortByMember(memberName)
-//      - reportByNeighbourhood()
-//      - reportByWard()
 
 int compareByTableId(const void *a, const void *b) 
 {
+    // To be used with qsort().
+    // Compare two tables by tableId and return difference.
+    // Parameters: Two void pointers to PicnicTable struct.
+
     PicnicTable *tableA = (PicnicTable *)a;
     PicnicTable *tableB = (PicnicTable *)b;
 
@@ -26,6 +20,10 @@ int compareByTableId(const void *a, const void *b)
 
 int compareBySiteId(const void *a, const void *b) 
 {
+    // To be used with qsort().
+    // Compare two tables by siteId and return difference.
+    // Parameters: Two void pointers to PicnicTable struct.
+
     PicnicTable *tableA = (PicnicTable *)a;
     PicnicTable *tableB = (PicnicTable *)b;
 
@@ -34,6 +32,10 @@ int compareBySiteId(const void *a, const void *b)
 
 int compareByTableTypeId(const void *a, const void *b) 
 {
+    // To be used with qsort().
+    // Compare two tables by tableTypeId and return difference.
+    // Parameters: Two void pointers to PicnicTable struct.
+
     PicnicTable *tableA = (PicnicTable *)a;
     PicnicTable *tableB = (PicnicTable *)b;
 
@@ -42,6 +44,10 @@ int compareByTableTypeId(const void *a, const void *b)
 
 int compareBySurfaceMaterialId(const void *a, const void *b) 
 {
+    // To be used with qsort().
+    // Compare two tables by surafaceMaterialId and return difference.
+    // Parameters: Two void pointers to PicnicTable struct.
+
     PicnicTable *tableA = (PicnicTable *)a;
     PicnicTable *tableB = (PicnicTable *)b;
 
@@ -50,6 +56,10 @@ int compareBySurfaceMaterialId(const void *a, const void *b)
 
 int compareByStructuralMaterialId(const void *a, const void *b) 
 {
+    // To be used with qsort().
+    // Compare two tables by structuralMaterialId and return difference.
+    // Parameters: Two void pointers to PicnicTable struct.
+
     PicnicTable *tableA = (PicnicTable *)a;
     PicnicTable *tableB = (PicnicTable *)b;
 
@@ -58,6 +68,10 @@ int compareByStructuralMaterialId(const void *a, const void *b)
 
 int compareByStreetAvenue(const void *a, const void *b) 
 {
+    // To be used with qsort().
+    // Compare two tables by streetAvenue and return info regarding alphebetical order.
+    // Parameters: Two void pointers to PicnicTable struct.
+
     PicnicTable *tableA = (PicnicTable *)a;
     PicnicTable *tableB = (PicnicTable *)b;
 
@@ -66,6 +80,10 @@ int compareByStreetAvenue(const void *a, const void *b)
 
 int compareByNeighbourhoodId(const void *a, const void *b) 
 {
+    // To be used with qsort().
+    // Compare two tables by neighbourhoodId and return difference.
+    // Parameters: Two void pointers to PicnicTable struct.
+
     PicnicTable *tableA = (PicnicTable *)a;
     PicnicTable *tableB = (PicnicTable *)b;
 
@@ -74,6 +92,10 @@ int compareByNeighbourhoodId(const void *a, const void *b)
 
 int compareByNeighbourhoodName(const void *a, const void *b) 
 {
+    // To be used with qsort().
+    // Compare two tables by neighbourhoodName and return info regarding alphebetical order.
+    // Parameters: Two void pointers to PicnicTable struct.
+
     PicnicTable *tableA = (PicnicTable *)a;
     PicnicTable *tableB = (PicnicTable *)b;
 
@@ -82,6 +104,10 @@ int compareByNeighbourhoodName(const void *a, const void *b)
 
 int compareByWard(const void *a, const void *b) 
 {
+    // To be used with qsort().
+    // Compare two tables by ward and return difference.
+    // Parameters: Two void pointers to PicnicTable struct.
+
     PicnicTable *tableA = (PicnicTable *)a;
     PicnicTable *tableB = (PicnicTable *)b;
 
@@ -90,6 +116,10 @@ int compareByWard(const void *a, const void *b)
 
 int compareByLatitude(const void *a, const void *b) 
 {
+    // To be used with qsort().
+    // Compare two tables by latitude and return info regarding alphebetical/numerical order.
+    // Parameters: Two void pointers to PicnicTable struct.
+
     PicnicTable *tableA = (PicnicTable *)a;
     PicnicTable *tableB = (PicnicTable *)b;
 
@@ -98,6 +128,10 @@ int compareByLatitude(const void *a, const void *b)
 
 int compareByLongitude(const void *a, const void *b) 
 {
+    // To be used with qsort().
+    // Compare two tables by longitude and return info regarding alphebetical/numerical order.
+    // Parameters: Two void pointers to PicnicTable struct.
+
     PicnicTable *tableA = (PicnicTable *)a;
     PicnicTable *tableB = (PicnicTable *)b;
 
@@ -105,11 +139,16 @@ int compareByLongitude(const void *a, const void *b)
 }
 
 void sortByMember(char *memberName) 
-// I think Table size will need to be global value that is continously updated.
 {
-    // Set tableSize to however it's setup in new struct
-    // int tableSize = 
+    // This function takes a member name from the PicnicTable struct
+    // and sorts the table accordingly using qsort which is supplied
+    // with specific comparison functions for each member name.
+    // Parameters: Pointer to first character of memberName string.
 
+    // Retrieve current table size from database.
+    int tableSize = Db->size_of_PtableTable;
+
+    // Determine specific compare function to use in qsort based off memberName.
     if (strcmp(memberName, "tableId") == 0) 
     {
         qsort(Db->picnicTableTable, tableSize, sizeof(PicnicTable), compareByTableId);
@@ -156,57 +195,163 @@ void sortByMember(char *memberName)
     }
 }
 
+char *tableCodeToString(int givenId, const char *tableName) 
+{
+    // Helper function to retrieve the string value associated
+    // with a specific Id/Index in the "Type" tables of the Database.
+    // Parameters: givenId int from PicnicTable and constant pointer to tableName.
+
+    // Double check tableName was given.
+    if (tableName == NULL) 
+    {
+        return NULL;
+    }
+
+    // Using if/else statements determine if given tableName fits any of the database tables.
+    if (strcmp(tableName, "tableTypeTable") == 0) 
+    {
+        // Double check givenId is valid (> 0 and within selected tableType length.)
+        if (givenId >= 0 && givenId < Db->size_tableTypeTable) 
+        {
+            // Return the value at the specic Id/Index of the selected table.
+            return Db->tableTypeTable->arr[givenId];
+        }
+    }
+    else if (strcmp(tableName, "surfaceMaterialTable") == 0) 
+    {
+        // Double check givenId is valid (> 0 and within selected tableType length.)
+        if (givenId >= 0 && givenId < Db->size_surfaceMaterialTable) 
+        {
+            // Return the value at the specic Id/Index of the selected table.
+            return Db->surfaceMaterialTable->arr[givenId];
+        }
+    }
+    else if (strcmp(tableName, "structuralMaterialTable") == 0) 
+    {
+        // Double check givenId is valid (> 0 and within selected tableType length.)
+        if (givenId >= 0 && givenId < Db->size_structuralMaterialTable) 
+        {
+            // Return the value at the specic Id/Index of the selected table.
+            return Db->structuralMaterialTable->arr[givenId];
+        }
+    }
+    else 
+    {
+        return NULL;
+    }
+}
+
 void reportByNeighbourhood() 
 {
-    // Set tableSize to however it's setup in new struct
-    // int tableSize = 
+    // Produces a listing of Picnic Tables grouped by neighbourhood
+    // in ascending alphebetical order, prints to terminal.
+    // Parameters: None
 
+    // Retrieve current table size from database.
+    int tableSize = Db->size_of_PtableTable;
+
+    // Use sortByMember to get table in alphebetical order.
     sortByMember("neighbourhoodName");
 
     printf("Report by Neighbourhood: \n");
 
+    // Temp string to hold what neighbourhood currently looking at.
+    // Begins as "empty" string so that first loop will print the first neighbourhood title.
     char selectedNeighbourhood[128] = "";
 
+    // Loop through all items in PicnicTable.
     for (int i = 0; i < tableSize; i++) 
     {
         PicnicTable *table = &Db->picnicTableTable[i];
 
-        // Check if in new neighbourhood to print out the change.
+        // Check If Neighbourhood has changed.
         if (strcmp(table->neighbourhoodName, selectedNeighbourhood) != 0) 
         {
+            // Update temp selectedNeighbourhood string.
             strcpy(selectedNeighbourhood, table->neighbourhoodName);
-            printf("\nNeighbourhood: %s\n", selectedNeighbourhood);
+            // Print Neighbourhood title.
+            printf("\n%s", selectedNeighbourhood);
+
+            // Prints the first item alongside the neighbourhood title,
+            // this is done to accomodate spacing consistency and look.
+            printf("\t\t   %-4d %-16s %-12s %-15s %-40s %-6d %-20s %-20s %-20s (%s, - %s)\n", 
+            table->siteId, 
+            tableCodeToString(table->tableTypeId, "tableTypeTable"),
+            tableCodeToString(table->surfaceMaterialId, "surfaceMaterialTable"),
+            tableCodeToString(table->structuralMaterialId, "structuralMaterialTable"),
+            table->streetAvenue,
+            table->neighbourhoodId,
+            table->neighbourhoodName,
+            table->latitude,
+            table->longitude,
+            table->latitude,
+            table->longitude);
+            continue;
         }
 
-        // Print Each Neighbourhood
-        printf("Table %d, %s, %s, Ward: %d, Latitude: %s, Longitude: %s\n", 
-            table->tableID, table->streetAvenue, table->neighbourhoodName, table->ward, table->latitude, table->longitude);
+        // Print each subseqent non-first item inside the neighbourhood.
+        printf("\t\t\t   %-4d %-16s %-12s %-15s %-40s %-6d %-20s %-20s %-20s (%s, - %s)\n", 
+            table->siteId, 
+            tableCodeToString(table->tableTypeId, "tableTypeTable"),
+            tableCodeToString(table->surfaceMaterialId, "surfaceMaterialTable"),
+            tableCodeToString(table->structuralMaterialId, "structuralMaterialTable"),
+            table->streetAvenue,
+            table->neighbourhoodId,
+            table->neighbourhoodName,
+            table->latitude,
+            table->longitude,
+            table->latitude,
+            table->longitude);
     }
 }
 
 void reportByWard() 
 {
-    // Set tableSize to however it's setup in new struct
-    // int tableSize = 
+    // Produces a listing of Picnic Tables grouped by ward
+    // in ascending numerical order, prints to terminal.
+    // Parameters: None
+
+    // Retrieve current table size from database.
+    int tableSize = Db->size_of_PtableTable;
     
+    // Use sortByMember to get table in numerical order.
     sortByMember("ward");
 
     printf("Report by Ward: \n");
 
-    int selectedWard = -1; // We start with -1 so the table will run ward print out on first go.
+    // Temp int to hold which ward currently looking at.
+    // Begins at -1 so that first loop will print the first ward title.
+    int selectedWard = -1;
 
+    // Loop through all items in PicnicTable.
     for (int i = 0; i < tableSize; i++) 
     {
         PicnicTable *table = &Db->picnicTableTable[i];
 
+        // Check If ward has changed.
         if (table->ward != selectedWard) 
         {
+            // Update temp selected ward.
             selectedWard = table->ward;
-            printf("\nWard: %d\n", selectedWard);
+            // Print ward title.
+            printf("\nWARD %d", selectedWard);
+            // NOTE: First print loop in this function does not need first item
+            //       to be printed within this if statement, since ward title is
+            //       much smaller than in the reportByNeighbourhood() function.
         }
 
-        // Print the Table info.
-        printf("Table %d, %s, %s, Neighbourhood: %s, Latitude: %s, Longitude %s\n", 
-            table->tableID, table->streetAvenue, table->neighbourhoodName, table->neighbourhoodName, table->latitude, table->longitude);
+        // Print each item within the selected ward.
+        printf("\t   %-4d %-16s %-12s %-15s %-40s %-6d %-20s %-20s %-20s (%s, - %s)\n", 
+            table->siteId, 
+            tableCodeToString(table->tableTypeId, "tableTypeTable"),
+            tableCodeToString(table->surfaceMaterialId, "surfaceMaterialTable"),
+            tableCodeToString(table->structuralMaterialId, "structuralMaterialTable"),
+            table->streetAvenue,
+            table->neighbourhoodId,
+            table->neighbourhoodName,
+            table->latitude,
+            table->longitude,
+            table->latitude,
+            table->longitude);
     }
 }
